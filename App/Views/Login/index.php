847aecb0e55bbb2ce76622fc6034a338b54301fb
Shiroganari <shiroganari@gmail.com>
2021-11-19 04:14:11 +0700

    Create Login/index view

---
+++ App/Views/Login/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kaigi | Signin</title>
+    <link rel="stylesheet" href="<?php ROOT ?>/styles/main.css">
+</head>
+<body>
+<?php require_once(dirname(__DIR__) . '/layouts/header.php'); ?>
+
+<section class="signup">
+    <h1 class="signup__title">Вход в аккаунт</h1>
+
+    <form class="form" method="post" action="http://kaigi.loc/register/signup">
+        <div class="form__item">
+            <label class="form__label" for="email">Введите ваш Email:</label>
+            <input class="form__input" id="email" name="email" type="email">
+        </div>
+
+        <div class="form__item">
+            <label class="form__label" for="pass">Введите пароль:</label>
+            <input class="form__input" id="pass" name="pass" type="password">
+        </div>
+
+        <input class="form__button button" type="submit" value="Войти в аккаунт">
+    </form>
+</section>
+</body>
+</html>

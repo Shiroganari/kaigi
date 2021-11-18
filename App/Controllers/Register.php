ccbffa629561fe289a09ca5ed01e61a3c887bff2
Shiroganari <shiroganari@gmail.com>
2021-11-19 02:39:50 +0700

    Create signupAction in Register controller

--- App/Controllers/Register.php
+++ App/Controllers/Register.php
@@ -5,10 +5,22 @@ namespace App\Controllers;
 use Core\Controller;
 use Core\View;
 
+use App\Models\User;
+
 class Register extends Controller
 {
     public function indexAction()
     {
         View::render('Register/index.php');
+    }
+
+    public function signupAction()
+    {
+        $firstName = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['pass'];
+        $location = 'Novosibirsk';
+
+        User::registerNewUser($firstName, $email, $password, $location);
     }
 }

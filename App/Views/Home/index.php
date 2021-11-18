0b29f0bbd74f988feb90ce31bdf86efc78af723f
Shiroganari <shiroganari@gmail.com>
2021-11-19 00:43:24 +0700

    Create header section

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -5,10 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
+    <title>Kaigi | Main</title>
     <link rel="stylesheet" href="<?php ROOT ?>/styles/main.css">
 </head>
 <body>
-    <h1>Hello, World!</h1>
+<?php require_once(dirname(__DIR__) . '/layouts/header.php'); ?>
 </body>
 </html>

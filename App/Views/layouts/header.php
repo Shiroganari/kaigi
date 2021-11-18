5d055adb1651450795d46959968358e3a9607322
Shiroganari <shiroganari@gmail.com>
2021-11-19 04:15:21 +0700

    Update links in header

--- App/Views/layouts/header.php
+++ App/Views/layouts/header.php
@@ -14,8 +14,8 @@
             </nav>
 
             <div class="header__buttons">
-                <a class="header__button" href="/account/signin">Войти</a>
-                <a class="header__button" href="/Register/index">Регистрация</a>
+                <a class="header__button" href="/login/index">Войти</a>
+                <a class="header__button" href="/register/index">Регистрация</a>
             </div>
         </div> <!-- /.header__inner -->
     </div> <!-- /.header -->

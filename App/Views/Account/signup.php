187eb57e1db26d599a3ca48681ab9662798a03a7
Shiroganari <shiroganari@gmail.com>
2021-11-19 01:13:20 +0700

    Update signup view

--- App/Views/Account/signup.php
+++ App/Views/Account/signup.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kaigi | Signup</title>
+    <link rel="stylesheet" href="<?php ROOT ?>/styles/main.css">
 </head>
 <body>
 <?php require_once(dirname(__DIR__) . '/layouts/header.php'); ?>

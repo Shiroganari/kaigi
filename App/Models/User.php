8fee0c3267995816220b5d84bd9181f0ce91d210
Shiroganari <shiroganari@gmail.com>
2021-11-19 16:36:36 +0700

    Create getUser method in User model

--- App/Models/User.php
+++ App/Models/User.php
@@ -33,4 +33,18 @@ class User extends Model
             echo $e->getMessage();
         }
     }
+
+    public static function getUser($email, $password)
+    {
+        try {
+            $db = static::getDB();
+
+            $stmt = $db->prepare("SELECT id FROM `users` WHERE email = '".$email."' AND password = '".$password."'");
+            $stmt->execute();
+            return $stmt->fetch();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit;
+        }
+    }
 }

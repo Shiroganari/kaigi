761e8c8cad4db19eff9239090adcd2da8b1d7481
Shiroganari <shiroganari@gmail.com>
2021-11-19 15:37:29 +0700

    Update register form fields

--- App/Views/Register/index.php
+++ App/Views/Register/index.php
@@ -16,18 +16,18 @@
 
         <form class="form" method="post" action="http://kaigi.loc/register/signup">
             <div class="form__item">
-                <label class="form__label" for="name">Введите ваше имя:</label>
-                <input class="form__input" id="name" name="name" type="text">
+                <label class="form__label" for="name">Введите ваш никнейм:</label>
+                <input class="form__input" id="name" name="username" type="text" placeholder="Никнейм">
             </div>
 
             <div class="form__item">
                 <label class="form__label" for="email">Введите ваш Email:</label>
-                <input class="form__input" id="email" name="email" type="email">
+                <input class="form__input" id="email" name="email" type="email" placeholder="Email">
             </div>
 
             <div class="form__item">
                 <label class="form__label" for="pass">Придумайте пароль:</label>
-                <input class="form__input" id="pass" name="pass" type="password">
+                <input class="form__input" id="pass" name="pass" type="password" placeholder="Пароль">
             </div>
 
             <input class="form__button button" type="submit" value="Создать аккаунт">

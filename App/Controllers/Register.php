d401b95697dfec25519f72cb14f6f557f3eb98b1
Shiroganari <shiroganari@gmail.com>
2021-11-22 12:55:22 +0700

    Update signupAction method of Register controller

--- App/Controllers/Register.php
+++ App/Controllers/Register.php
@@ -19,19 +19,24 @@ class Register extends Controller
         $username = $this->post_params['username'];
         $email = $this->post_params['email'];
         $password = $this->post_params['pass'];
-        $location = 'Novosibirsk';
 
-        if ($this->validate($username, $email, $password) == 'OK') {
-            User::registerNewUser($username, $email, $password, $location);
-            echo 'Регистрация прошла успешно!';
+        if ($this->validate($username, $email, $password) == 'ERROR') {
+            echo 'Пожалуйста, проверьте корректность введенных Вами данных.';
+            exit;
         }
+
+        if (User::checkUser($username, $email)) {
+            echo 'Такой пользователь уже существует.';
+            exit;
+        }
+
+        User::registerNewUser($username, $email, $password);
     }
 
     protected function validate($username, $email, $password)
     {
         if (!$username || !$email || !$password) {
-           echo 'Ошибка. Введенные данные пользователя не прошли валидацию';
-           exit;
+           return 'ERROR';
         }
 
         return 'OK';

494c92256fa880aff3234d6229306b546ed2d0c0
Shiroganari <shiroganari@gmail.com>
2021-12-24 16:02:08 +0700

    Core\QueryBuilder | Complete refactoring QueryBuilder

--- Core/QueryBuilder.php
+++ Core/QueryBuilder.php
@@ -4,83 +4,45 @@ namespace Core;
 
 class QueryBuilder extends Database
 {
-    private array $table = [];
-    private array $select = [];
-    private array $insert = [];
-    private array $where = [];
-    private array $update = [];
-    private array $orWhere = [];
+    private string $table = '';
+    private string $select = '';
+    private string $insert = '';
+    private string $update = '';
+    private string $where = '';
+    private string $orWhere = '';
+
+    private bool $max = false;
+    private bool $like = false;
     private bool $delete = false;
-    private bool $isMax = false;
 
     public function __toString(): string
     {
         $query = '';
-        $where = '';
-        $orWhere = '';
 
-        if ($this->where !== []) {
-            $where = ' WHERE ';
-
-            foreach ($this->where as $key => $value) {
-                $where .= $key . ' = ' . "'$value'" . ' AND ';
-            }
-
-            $where = rtrim($where, ' AND ');
-        } elseif ($this->orWhere !== []) {
-            $orWhere = ' WHERE ';
-
-            foreach ($this->orWhere as $key => $value) {
-                $orWhere .= $key . ' = ' . "'$value'" . ' OR ';
-            }
-
-            $orWhere = rtrim($orWhere, ' OR ');
-        }
-
-        if ($this->select !== []) {
-            if ($this->isMax) {
-                $query = 'SELECT MAX(' . implode(', ', $this->select) . ')';
+        if ($this->select !== '') {
+            if ($this->max) {
+                $query = "SELECT MAX($this->select)";
             } else {
-                $query = 'SELECT ' . implode(', ', $this->select);
+                $query = "SELECT $this->select";
             }
 
-            $query .= ' FROM ' . implode(', ', $this->table);
-            $query .= $where;
-            $query .= $orWhere;
-        } elseif ($this->insert !== []) {
-            $query .= 'INSERT INTO ' . implode(', ', $this->table) . '(';
-
-            foreach ($this->insert as $key => $value) {
-                $query .= $key . ', ';
-            }
-
-            $query = rtrim($query, ', ');
-            $query .= ')';
-            $query .= ' VALUES (';
-
-            foreach ($this->insert as $value) {
-                $query .= "'$value'" . ', ';
-            }
-
-            $query = rtrim($query, ', ');
-            $query .= ')';
+            $query .= " FROM $this->table";
+            $query .= $this->where;
+            $query .= $this->orWhere;
+        } elseif ($this->insert !== '') {
+            $query .= "INSERT INTO $this->table";
+            $query .= $this->insert;
+            $query .= $this->where;
+            $query .= $this->orWhere;
         } elseif ($this->delete) {
-            $query = 'DELETE FROM ' . implode(', ', $this->table);
-            $query .= $where;
-            $query .= $orWhere;
-        } elseif($this->update !== []) {
-            $query .= 'UPDATE ' . implode(', ', $this->table) . ' SET ';
-
-            foreach ($this->update as $key => $value) {
-                if ($value) {
-                    $query .= $key . ' = ' . "'$value'" . ', ';
-                }
-            }
-
-            $query = rtrim($query, ', ');
-
-            $query .= $where;
-            $query .= $orWhere;
+            $query = "DELETE FROM $this->table";
+            $query .= $this->where;
+            $query .= $this->orWhere;
+        } elseif($this->update !== '') {
+            $query .= "UPDATE $this->table SET ";
+            $query .= $this->update;
+            $query .= $this->where;
+            $query .= $this->orWhere;
         }
 
         return $query;
@@ -88,70 +50,118 @@ class QueryBuilder extends Database
 
     public function table(string $table): self
     {
-        $this->table[] = $table;
+        $this->table = $table;
         return $this;
     }
 
     public function select(string ...$select): self
     {
-        $this->select = $select;
+        $this->select = implode(', ', $select);
         return $this;
     }
 
     public function insert(array $insert): self
     {
+        $insertQuery = '(';
+
         foreach ($insert as $key => $value) {
-            $this->insert[$key] = $value;
+            $insertQuery .= $key . ', ';
         }
+
+        $insertQuery = rtrim($insertQuery, ', ');
+        $insertQuery .= ')';
+        $insertQuery .= ' VALUES (';
+
+        foreach ($insert as $value) {
+            $insertQuery .= "'$value'" . ', ';
+        }
+
+        $insertQuery = rtrim($insertQuery, ', ');
+        $insertQuery .= ')';
+        $this->insert = $insertQuery;
 
         return $this;
     }
 
     public function update(array $update): self
     {
+        $updateQuery = '';
+
         foreach ($update as $key => $value) {
-            $this->update[$key] = $value;
+            if ($value) {
+                $updateQuery .= $key . ' = ' . "'$value'" . ', ';
+            }
         }
+
+        $updateQuery = rtrim($updateQuery, ', ');
+        $this->update = $updateQuery;
 
         return $this;
     }
 
     public function where(array $where): self
     {
+        $whereQuery = ' WHERE ';
+
         foreach ($where as $key => $value) {
-            $this->where[$key] = $value;
+            $whereQuery .= $key;
+
+            if ($this->like) {
+                $whereQuery .= ' LIKE ';
+                $whereQuery .= "'%$value%'";
+            } else {
+                $whereQuery .= ' = ';
+                $whereQuery .= "'$value'";
+            }
+
+            $whereQuery .= ' AND ';
         }
+
+        $whereQuery = rtrim($whereQuery, ' AND ');
+        $this->where = $whereQuery;
 
         return $this;
     }
 
     public function orWhere(array $orWhere): self
     {
+        $orWhereQuery = ' WHERE ';
+
         foreach ($orWhere as $key => $value) {
-            $this->orWhere[$key] = $value;
+            $orWhereQuery .= $key;
+
+            if ($this->like) {
+                $orWhereQuery .= ' LIKE ';
+                $orWhereQuery .= "'%$value%'";
+            } else {
+                $orWhereQuery .= ' = ';
+                $orWhereQuery .= "'$value'";
+            }
+
+            $orWhereQuery .= ' OR ';
         }
 
+        $orWhereQuery = rtrim($orWhereQuery, ' OR ');
+        $this->orWhere = $orWhereQuery;
+
         return $this;
     }
 
-    public function setParams(array $params): self
+    public function max(): self
     {
-        foreach ($params as $key => $value) {
-            $this->params[$key] = $value;
-        }
-
+        $this->max = true;
         return $this;
     }
 
-    public function max()
-    {
-        $this->isMax = true;
-        return $this;
-    }
-
-    public function delete()
+    public function delete(): self
     {
         $this->delete = true;
+        return $this;
+    }
+
+    public function like(): self
+    {
+        $this->like = true;
         return $this;
     }
 }

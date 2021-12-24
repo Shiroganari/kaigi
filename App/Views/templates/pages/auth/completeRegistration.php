a41b41b2f7ef5b0944af0df9cd5715e31026d649
Shiroganari <shiroganari@gmail.com>
2021-12-24 14:59:53 +0700

    Views\auth | Add required attribute to form fields

--- App/Views/templates/pages/auth/completeRegistration.php
+++ App/Views/templates/pages/auth/completeRegistration.php
@@ -14,26 +14,26 @@
 
         <div class="form__item">
             <label class="form__label" for="description">Расскажите что-нибудь о себе: </label>
-            <textarea class="form__textarea" name="description" id="description" cols="30" rows="10"></textarea>
+            <textarea class="form__textarea" name="description" id="description" cols="30" rows="10" required></textarea>
         </div> <!-- /.form__item -->
 
         <div class="form__item">
             <label for="countryId" class="form__label">Выберите страну</label>
-            <select class="form__select countries order-alpha" id="countryId" name="country">
+            <select class="form__select countries order-alpha" id="countryId" name="country" required>
                 <option value="">Select Country</option>
             </select>
         </div> <!-- /.form__item -->
 
         <div class="form__item">
             <label for="stateId" class="form__label">Выберите область</label>
-            <select class="form__select states" id="stateId" name="state">
+            <select class="form__select states" id="stateId" name="state" required>
                 <option value="">Select State</option>
             </select>
         </div> <!-- /.form__item -->
 
         <div class="form__item">
             <label for="cityId" class="form__label">Выберите город</label>
-            <select class="form__select cities" id="cityId" name="city">
+            <select class="form__select cities" id="cityId" name="city" required>
                 <option value="">Select City</option>
             </select>
         </div> <!-- /.form__item -->
--- App/Views/templates/pages/auth/registration.php
+++ App/Views/templates/pages/auth/registration.php
@@ -4,17 +4,17 @@
     <form class="form" method="post" action="auth/registration">
         <div class="form__item">
             <label class="form__label" for="name">Введите ваш никнейм:</label>
-            <input class="form__input" id="name" name="username" type="text" placeholder="Никнейм">
+            <input class="form__input" id="name" name="username" type="text" placeholder="Никнейм" required>
         </div>
 
         <div class="form__item">
             <label class="form__label" for="email">Введите ваш Email:</label>
-            <input class="form__input" id="email" name="email" type="email" placeholder="Email">
+            <input class="form__input" id="email" name="email" type="email" placeholder="Email" required>
         </div>
 
         <div class="form__item">
             <label class="form__label" for="pass">Придумайте пароль:</label>
-            <input class="form__input" id="pass" name="pass" type="password" placeholder="Пароль">
+            <input class="form__input" id="pass" name="pass" type="password" placeholder="Пароль" required>
         </div>
 
         <input class="form__button button" type="submit" value="Создать аккаунт">
